85f2d1f2593b04d4f7172f04bd15b72606b2d184
Nithin0620 <nithin30302@gmail.com>
2025-06-29 01:16:59 +0530

    .

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 const cors = require("cors")
 const PORT = process.env.PORT || 4000 || 8000
 
-app.use(cors({
-  origin: "http://localhost:3000", // your frontend URL
-  credentials: true, // <-- Allow credentials (cookies)
-}));
+// app.use(cors({
+//   origin: "http://localhost:3000", // your frontend URL
+//   credentials: true, // <-- Allow credentials (cookies)
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
