660dcf1f6d672406a3cb1694f8cf915bd4009998
Nithin0620 <nithin30302@gmail.com>
2025-06-29 01:32:03 +0530

    .

--- src/services/apis.jsx
+++ src/services/apis.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.MODE  === "development" ?"http://localhost:5000/api/v1" : "https://events-planner-8hqj.onrender.com/api/v1";
+const BASE_URL = process.env.NODE_ENV === "development" ?"http://localhost:5000/api/v1" : "https://events-planner-8hqj.onrender.com/api/v1";
 
 export const auth = {
    logIn: `${BASE_URL}/login`,
